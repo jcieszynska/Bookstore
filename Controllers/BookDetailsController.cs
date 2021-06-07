1738c26e7aac9c50bf6f54214c54375ad624a65e
Joanna Cieszynska <joannacieszynska33@gmail.com>
2021-06-07 15:55:24 +0200

    Initial commit

---
+++ Controllers/BookDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bookstore.Models;
+
+namespace Bookstore.Controllers
+{
+    public class BookDetailsController : Controller
+    {
+        private bookstoredbEntities db = new bookstoredbEntities();
+
+        // GET: BookDetails
+        public ActionResult Index()
+        {
+            return View(db.BookDetail.ToList());
+        }
+
+        // GET: BookDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookDetail bookDetail = db.BookDetail.Find(id);
+            if (bookDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookDetail);
+        }
+
+        // GET: BookDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookDetails/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookID,BookTitle,BookAuthor,BookDescription,BookPrice,BookRating")] BookDetail bookDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BookDetail.Add(bookDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bookDetail);
+        }
+
+        // GET: BookDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookDetail bookDetail = db.BookDetail.Find(id);
+            if (bookDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookDetail);
+        }
+
+        // POST: BookDetails/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookID,BookTitle,BookAuthor,BookDescription,BookPrice,BookRating")] BookDetail bookDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bookDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bookDetail);
+        }
+
+        // GET: BookDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BookDetail bookDetail = db.BookDetail.Find(id);
+            if (bookDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookDetail);
+        }
+
+        // POST: BookDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BookDetail bookDetail = db.BookDetail.Find(id);
+            db.BookDetail.Remove(bookDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
